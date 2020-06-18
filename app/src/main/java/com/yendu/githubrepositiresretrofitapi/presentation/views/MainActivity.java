c3a68b65cefd115c88e092186931b937e05a7bca
yonatan <yendu669@gmail.com>
2020-06-18 12:20:14 +0300

    Create the app

---
+++ app/src/main/java/com/yendu/githubrepositiresretrofitapi/presentation/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yendu.githubrepositiresretrofitapi.presentation.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yendu.githubrepositiresretrofitapi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
